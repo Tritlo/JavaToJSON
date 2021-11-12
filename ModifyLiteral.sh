8f3f4f928bc17d3c5d9cb0f1047440a2abaa62e2
Matthías Páll Gissurarson <mpg@mpg.is>
2021-11-12 10:05:52 +0000

    WIP

---
+++ ModifyLiteral.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+#To set it to e.g. "goodbye, world", you must pass it '\"goodbye, world\"'
+ORIG=$(jq)
+#Make sure we only modify literals (e.g. the "type check") by filtering out
+#non literals.
+FILTERED=$(echo $ORIG | jq "select(.type? == \"Literal\") ")
+if [[ ! -z "$FILTERED" ]];
+then
+    echo $ORIG
+    RES=$(echo $FILTERED | jq ".pretty_printed = \"$1\"")
+    echo $RES
+    jq --null-input --argjson NEW "$RES" --argjson OLD "$FILTERED" '{"old": $OLD, "new": $NEW}'
+fi
