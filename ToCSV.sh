731738054848a32aed6808c1c880d4df03c549c6
Matthías Páll Gissurarson <mpg@mpg.is>
2021-11-18 15:52:00 +0000

    Echo header

--- ToCSV.sh
+++ ToCSV.sh
@@ -4,6 +4,7 @@
 #      | .children[].parent = .location | .children[].parent.name = .spoon_class | ., .children[] | select(.parent?) | del(.children) | (. | select(.parent.name == null)), (select(.parent.name?) | (.parent.name |= (split(".") | .[-1] | split("$") | .[-1]))) | [.spoon_class, .location?.start_line, .location?.end_line, .location?.start_col, .location?.end_col, .location?.file, .label, .parent?.start_line, .parent?.end_line, .parent?.start_col, .parent?.end_col, .parent?.name] | @csv' -r
 
 
+echo "NAME,LINE_START,LINE_END,COLUMN_START,COLUMN_END,REL_PATH,VALUE,PARENT_LINE_START,PARENT_LINE_END,PARENT_COLUMN_START,PARENT_COLUMN_END,PARENT_NAME,VISIBILITY"
 jq '(.. | select(.children?) | select(.spoon_class?)
        | (.spoon_class |= (split(".") | .[-1] | split("$") | .[-1]))
        | .children[].spoon_class |= (split(".") | .[-1] | split("$") | .[-1])
